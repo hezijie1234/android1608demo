2bf940a2eda24458c243d11ba835e50f3c49028b
yangjw <yangwen8701@126.com>
2017-02-06 16:19:09 +0800

    okhttp的Post请求方式demo

--- day33_okhttp_post_demo/src/main/java/com/androidxx/yangjw/day33_okhttp_post_demo/MainActivity.java
+++ day33_okhttp_post_demo/src/main/java/com/androidxx/yangjw/day33_okhttp_post_demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.androidxx.yangjw.day33_okhttp_post_demo;
 
 import android.os.Handler;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,8 +14,10 @@ import java.lang.ref.WeakReference;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -49,16 +52,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void click(View view) {
-        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+        jsonParamToServer();
+    }
+
+
+    /**
+     * 传递Form表单格式的数据
+     */
+    public void formDataToServer() {
+                OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .build();
         FormBody formBody = new FormBody.Builder()
-                .add("pageno","1") //键值对的形式保存参数（需要传递到后台服务器的参数）
+                .add("name","zhangsan") //键值对的形式保存参数（需要传递到后台服务器的参数）
+                .add("age","33")
                 .build();
         Request request = new Request.Builder()
-                .url("http://www.1688wan.com/majax.action?method=getGiftList")
+                .url("http://192.168.53.18:8080/Android1608/login")
                 .post(formBody) //需要传值
                 .build();
 
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result = response.body().string();
+                Message message = myHandler.obtainMessage();
+                message.obj = result;
+                message.sendToTarget();
+            }
+        });
+    }
+
+
+    /**
+     * 传递Json格式的数据
+     */
+    public void jsonParamToServer() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .build();
+        /**
+         * 参数1：传递的数据类型（MIME，image/jpeg,application/json）
+         * 参数2：需要传递的数据
+         */
+        MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
+        RequestBody requestBody = RequestBody.create(mediaType,"{name:zhangsan,age:38}");
+        Request request = new Request.Builder()
+                .url("http://192.168.53.18:8080/Android1608/login")
+                .post(requestBody)
+                .build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
             @Override
