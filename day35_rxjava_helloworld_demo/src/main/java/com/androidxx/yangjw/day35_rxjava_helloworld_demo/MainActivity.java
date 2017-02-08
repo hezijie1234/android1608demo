ca5e78557ace29466850c8e6c80e0db527298aec
yangjw <yangwen8701@126.com>
2017-02-08 10:20:08 +0800

    rxjava

---
+++ day35_rxjava_helloworld_demo/src/main/java/com/androidxx/yangjw/day35_rxjava_helloworld_demo/MainActivity.java
@@ -0,0 +1,80 @@
+package com.androidxx.yangjw.day35_rxjava_helloworld_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+
+/**
+ * Rxjava的基本使用
+ * 步骤：
+ * 1、导包  compile 'io.reactivex:rxjava:1.2.6'
+ * 2、创建一个观察者
+ * 3、创建一个被观察者
+ * 4、关联观察者和被观察者
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        //观察者对象
+        //泛型的含义：表示此对象中操作的数据类型只能是String类型
+        Observer<String> observer = new Observer<String>() {
+
+            /**
+             * 完成：此方法是一个终点。表示观察与被观察的一个结束。
+             */
+            @Override
+            public void onCompleted() {
+
+            }
+
+            /**
+             * 当被观察者出现异常，则执行此方法
+             * @param e
+             */
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            /**
+             * 观察者用来接收被观察者发送的消息
+             * @param s 被观察者发送的消息
+             */
+            @Override
+            public void onNext(String s) {
+                Log.i("androidxx", "onNext: " + s);
+            }
+        };
+
+        //被观察者对象
+        Observable.OnSubscribe<String> subscribe = new Observable.OnSubscribe<String>() {
+
+            /**
+             * 触发观察者的方法
+             * @param subscriber 用来和观察者进行通信的一个对象
+             */
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("zhangsan");
+                subscriber.onCompleted();
+            }
+        };
+
+        Observable<String> observable = Observable.create(subscribe);
+
+        //关联,触发被观察者对象中的call方法执行的语句
+        observable.subscribe(observer);
+    }
+
+}
